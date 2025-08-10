485d89d9b6c92856fde418ab91e07cfe63aa71e0
Sahilabbas123 <asxfchannel@gmail.com>
2025-08-10 16:42:40 +0530

    setup for movie website done and created some componenets

---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+// Search.jsx
+import React from 'react';
+
+
+const Search = (props) => {
+
+  return (
+    <div className='text-white text-3xl'>{props.searchTerm}</div>
+  );
+};
+
+export default Search;
